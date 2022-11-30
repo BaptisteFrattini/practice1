e1b77bb1ed266d1adb500717523e9f40b58954a0
Baptiste Frattini <baptiste.frattini22@gmail.com>
2022-11-30 10:30:40 +0100

    debut code

---
+++ analyses/Exercice.R
@@ -0,0 +1,4 @@
+readr::read_csv("~/Downloads/practice1/data/wildfinder/wildfinder-ecoregions_list.csv")
+readr::read_csv("~/Downloads/practice1/data/wildfinder/wildfinder-ecoregions_species.csv")
+readr::read_csv("~/Downloads/practice1/data/wildfinder/wildfinder-mammals_list.csv")
+readr::read_csv("~/Downloads/practice1/data/pantheria/PanTHERIA_1-0_WR05_Aug2008.txt")
